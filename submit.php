bf4be5d7bfd378c2e3204ddfc82cf2816a8d9a5f
Nisarg Patel <nisarg1792@gmail.com>
2017-11-06 16:56:31 -0500

    Updated Contact form : submit

--- submit.php
+++ submit.php
@@ -1,41 +1,36 @@
-<?php
-	if (isset($_POST["submit"])) {
-		$name = $_POST['name'];
-		$email = $_POST['email'];
-		$phone = $_POST['phone'];
-		$message = $_POST['message'];
-		$from = 'noreply@gmailcom'; 
-		$to = ''; 
-		$subject = 'Message from Destination ';
-		
-		$body = "From: $name\n E-Mail: $email\n Message:\n $message";
- 
-		// Check if name has been entered
-		if (!$_POST['name']) {
-			$errName = 'Please enter your name';
-		}
-		
-		// Check if email has been entered and is valid
-		if (!$_POST['email'] || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
-			$errEmail = 'Please enter a valid email address';
-		}
-		
-		//Check if message has been entered
-		if (!$_POST['message']) {
-			$errMessage = 'Please enter your message';
-		}
-		//Check if simple anti-bot test is correct
-		if (!$_POST['phone']) {
-			$errPhone = 'Phone number is incorrect';
-		}
- 
-// If there are no errors, send the email
-if (!$errName && !$errEmail && !$errMessage && !$errPhone) {
-	if (mail ($to, $subject, $body, $from)) {
-		$result='<div class="alert alert-success">Thank You! I will be in touch</div>';
-	} else {
-		$result='<div class="alert alert-danger">Sorry there was an error sending your message. Please try again later</div>';
-	}
+<?php 
+  $to = 'nisarg1792@gmail.com' ;  // Replace email address with your own
+  $subject = 'Contact Form' ; 
+  $name = $_POST['name'] ; 
+  $email = $_POST['email'] ;
+  $phone = $_POST['phone'] ;
+  $message = $_POST['message'] ;
+
+  // The above are the variables that will hold data entered by the user
+  // You can use these variables, rename them or create your own
+  //Make sure the text in the square brackets correspond to the name attributes in your form
+
+
+  $message = "From: $name \nEmail: $email \nPhone: $phone \nMessage: $message \n";
+
+  // The $message variable will form the message of the email.
+  // It collects the variables your declared at the top
+  // A line break ( \n ) is placed between each section
+
+
+  $sent = mail($to, $subject, $message) ; 
+    if($sent) {
+      echo "<h3>Thank you for your message.</h3><p>Your email has been sent successfully and we appreciate you getting in touch with us. We will be sending a reply soon.</p>"; 
+    }else{
+      echo "<h3>Sorry, your message wasn't sent.</h3><p>We seem to have encountered a problem sending your message. Please go back and try again. If you get this message again please email <a href='mailto:webmaster@yourDomain.com'>webmaster@yourDomain.com</a>.</p>"; 
 }
-	}
+
+  // A new variable called $sent is created
+  // This variable uses the mail() function to send the data.
+  // The first option is the email address the message is sent to, as declared in the $to variable
+  // The second is the subject, as declared with the $subject variable
+  // The last is the message, as declared by the $message variable which holds the user submitted data
+
+  // If the message is sent successfully, a "success message" is echoed.
+  // If not, an alternate message is echoed.
 ?>
