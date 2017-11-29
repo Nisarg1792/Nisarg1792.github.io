735ddd0e0445c06ec60e3c27ca079fe42df99c4d
Nisarg Patel <nisarg1792@gmail.com>
2017-11-29 10:03:42 -0500

    Update submit.php

--- submit.php
+++ submit.php
@@ -21,13 +21,11 @@
     }else{
       echo "<h3>Sorry, your message wasn't sent.</h3><p>We seem to have encountered a problem sending your message. Please go back and try again. If you get this message again please email <a href='mailto:webmaster@yourDomain.com'>webmaster@yourDomain.com</a>.</p>"; 
 }
-
   // A new variable called $sent is created
   // This variable uses the mail() function to send the data.
   // The first option is the email address the message is sent to, as declared in the $to variable
   // The second is the subject, as declared with the $subject variable
   // The last is the message, as declared by the $message variable which holds the user submitted data
-
   // If the message is sent successfully, a "success message" is echoed.
   // If not, an alternate message is echoed.
 ?>
