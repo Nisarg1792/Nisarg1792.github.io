4f1b04b5ee8e08f313e17afb880388c459f38c79
Nisarg Patel <nisarg1792@gmail.com>
2018-03-16 11:26:02 -0400

    Update contact.js

--- js/contact.js
+++ js/contact.js
@@ -19,7 +19,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "mail/submit.php",
+                 url: "././mail/submit.php",
                 type: "POST",
                 data: {
                     name: name,
